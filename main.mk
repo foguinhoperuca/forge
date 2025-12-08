a7df33494268295c414dd89315eb06d5620aac53
Jefferson Campos <jefferson@jeffersoncampos.eti.br>
2025-12-07 22:08:27 -0300

    fix: Correct Changes.
    Correct patches added.
    Fix bad commit.

--- main.mk
+++ main.mk
@@ -25,73 +25,6 @@ patch-git-upstream: PATCH_GIT_TARGET=$(APP_PATH_UPSTREAM)
 patch-git-upstream: PATCH_GIT_DIFF_FILE_LOCATION=$(APP_PATH_WORKTREE)
 patch-git-upstream: patch-git
 
-<<<<<<< Updated upstream
-# # TODO cp secrets to /etc/adc - move it to forge.sh
-# # TODO set array var to credential's files.
-# cp-secrets:
-# 	@clear
-# 	@echo "|+-------------+|"
-# 	@echo "| COPY SECRETS  |"
-# 	@echo "|+-------------+|"
-# 	@date
-# 	@rm -f .*~
-# 	@rm -f *~
-# 	@rm -f .credentials/.*~
-# 	@rm -f .credentials/*~
-# 	scp .credentials/.mise-en-place.conf .credentials/.env.* .credentials/.google-service-account* .credentials/.pgpass.* .credentials/.target-server.* $(TARGET_SERVER_USER)@$(TARGET_SERVER_ADDR):$(shell echo "${APP_PATH_ORIGIN_EDGE}" | sed -e "s/${USER}/${TARGET_SERVER_USER}/g")/.credentials/
-# 	scp .credentials/.env.* .credentials/.google-service-account* .credentials/.pgpass.* .credentials/.target-server.* $(TARGET_SERVER_USER)@$(TARGET_SERVER_ADDR):$(APP_PATH_ETC)/
-# # ssh $(TARGET_SERVER_USER)@$(TARGET_SERVER_ADDR) "cd $(APP_PATH_ETC); source ./forge.sh export $(TARGET_ENV); ./forge.sh set_symbolic_link"
-# 	@date
-||||||| Stash base
-cp-secrets:
-	@clear
-	@echo "|+--------------------+|"
-	@echo "|  FORGE COPY SECRETS  |"
-	@echo "|+--------------------+|"
-	@date
-	rm -f .*~
-	rm -f *~
-	rm -f .credentials/$(APP_PATH_CREDENTIALS_GENERATED_OUTPUT)/.*~
-	rm -f .credentials/$(APP_PATH_CREDENTIALS_GENERATED_OUTPUT)/*~
-	@./forge.sh genenv all gpg
-	@tree -a .credentials/$(APP_PATH_CREDENTIALS_GENERATED_OUTPUT)/
-	@echo "---"
-	@cat .credentials/$(APP_PATH_CREDENTIALS_GENERATED_OUTPUT)/deployment_datetime.txt
-	@./forge.sh cp-secrets all
-	@tree -a $(APP_PATH_ETC)
-	@echo "==="
-	@cat $(APP_PATH_ETC)/deployment_datetime.txt
-	@date
-
-document_root:
-	@clear
-	@date
-	@rm "$(APP_PATH_DOCUMENT_ROOT)"
-	ln -sf "$(APP_PATH_WORKTREE)/$(TARGET_ENV)" "$(APP_PATH_DOCUMENT_ROOT)"
-	@ls -lah --color=auto "$(APP_PATH_WORKTREE)"
-
-post-receive:
-	@clear
-	@date
-	echo "abcdef123 fedcba987 refs/heads/$(TARGET_ENV)" | ./git-hooks/post-receive
-	@date
-
-SEARCH_SRC_STR ?= "FORGE_SYSTEM_NAME"
-SEARCH_TYPE ?= "SUMMARY"
-search-in-source-code:
-	@clear
-	@date
-	@echo "SEARCH_TYPE --> $(SEARCH_TYPE) ::: SEARCH SRC STR --> $(SEARCH_SRC_STR)"
-ifeq ($(SEARCH_TYPE),SUMMARY)
-	@grep -rn "$(SEARCH_SRC_STR)" * --exclude-dir={forge,tmp,venv,__pycache__,tests} --exclude={TAGS,dev.patch} | awk '{print $1}'
-else ifeq ($(SEARCH_TYPE),FULL)
-	@grep -rn "$(SEARCH_SRC_STR)" * --exclude-dir={forge,tmp,venv,__pycache__,tests} --exclude={TAGS,dev.patch} | awk '{print $1}' | grep -v "~" | grep -v ":from" | sort | uniq
-endif
-	@echo "-------"
-	@grep -rn "$(SEARCH_SRC_STR)" * --exclude-dir={forge,tmp,venv,__pycache__,tests} --exclude={TAGS,dev.patch} | awk '{print $1}' | sort | uniq | wc -l
-	@echo ""
-	@date
-=======
 cp-secrets:
 	@clear
 	@echo "|+--------------------+|"
@@ -148,7 +81,6 @@ endif
 	@grep -rn "$(SEARCH_SRC_STR)" * --exclude-dir={forge,tmp,venv,__pycache__,tests} --exclude={TAGS,dev.patch} | awk '{print $1}' | sort | uniq | wc -l
 	@echo ""
 	@date
->>>>>>> Stashed changes
 
 # deploy-apache-conf:
 # 	@echo "|+-------------+|"
@@ -158,11 +90,11 @@ endif
 # 	@rm -f webserver/apache/$(TARGET_SERVER_TYPE)_server/*~*
 # 	@ssh $(TARGET_SERVER_USER)@$(TARGET_SERVER_ADDR) "rm /home/$(TARGET_SERVER_USER)/tmp/$(APP_NAME)/*; mkdir -p /home/$(TARGET_SERVER_USER)/tmp/$(APP_NAME)/"
 # ifeq ($(TARGET_ENV),prod)
-# 	scp webserver/apache/$(TARGET_SERVER_TYPE)_server/$(APP_NAME).sorocaba.sp.gov.br.conf $(TARGET_SERVER_USER)@$(TARGET_SERVER_ADDR):/home/$(TARGET_SERVER_USER)/tmp/$(APP_NAME)/$(APP_NAME).sorocaba.sp.gov.br.conf
-# 	scp webserver/apache/$(TARGET_SERVER_TYPE)_server/$(APP_NAME)-api.sorocaba.sp.gov.br.conf $(TARGET_SERVER_USER)@$(TARGET_SERVER_ADDR):/home/$(TARGET_SERVER_USER)/tmp/$(APP_NAME)/$(APP_NAME)-api.sorocaba.sp.gov.br.conf
+# 	scp webserver/apache/$(TARGET_SERVER_TYPE)_server/$(APP_NAME).$FORGE_ORGANIZATION_BASEDNS.conf $(TARGET_SERVER_USER)@$(TARGET_SERVER_ADDR):/home/$(TARGET_SERVER_USER)/tmp/$(APP_NAME)/$(APP_NAME).$FORGE_ORGANIZATION_BASEDNS.conf
+# 	scp webserver/apache/$(TARGET_SERVER_TYPE)_server/$(APP_NAME)-api.$FORGE_ORGANIZATION_BASEDNS.conf $(TARGET_SERVER_USER)@$(TARGET_SERVER_ADDR):/home/$(TARGET_SERVER_USER)/tmp/$(APP_NAME)/$(APP_NAME)-api.$FORGE_ORGANIZATION_BASEDNS.conf
 # else
-# 	scp webserver/apache/$(TARGET_SERVER_TYPE)_server/$(APP_NAME).sorocaba.sp.gov.br.conf $(TARGET_SERVER_USER)@$(TARGET_SERVER_ADDR):/home/$(TARGET_SERVER_USER)/tmp/$(APP_NAME)/$(APP_NAME)-$(TARGET_ENV).sorocaba.sp.gov.br.conf
-# 	scp webserver/apache/$(TARGET_SERVER_TYPE)_server/$(APP_NAME)-api.sorocaba.sp.gov.br.conf $(TARGET_SERVER_USER)@$(TARGET_SERVER_ADDR):/home/$(TARGET_SERVER_USER)/tmp/$(APP_NAME)/$(APP_NAME)-api-$(TARGET_ENV).sorocaba.sp.gov.br.conf
+# 	scp webserver/apache/$(TARGET_SERVER_TYPE)_server/$(APP_NAME).$FORGE_ORGANIZATION_BASEDNS.conf $(TARGET_SERVER_USER)@$(TARGET_SERVER_ADDR):/home/$(TARGET_SERVER_USER)/tmp/$(APP_NAME)/$(APP_NAME)-$(TARGET_ENV).$FORGE_ORGANIZATION_BASEDNS.conf
+# 	scp webserver/apache/$(TARGET_SERVER_TYPE)_server/$(APP_NAME)-api.$FORGE_ORGANIZATION_BASEDNS.conf $(TARGET_SERVER_USER)@$(TARGET_SERVER_ADDR):/home/$(TARGET_SERVER_USER)/tmp/$(APP_NAME)/$(APP_NAME)-api-$(TARGET_ENV).$FORGE_ORGANIZATION_BASEDNS.conf
 # endif
 # 	@ssh $(TARGET_SERVER_USER)@$(TARGET_SERVER_ADDR) "sudo cp /home/$(TARGET_SERVER_USER)/tmp/$(APP_NAME)/$(APP_NAME)* /etc/apache2/sites-available/; sudo apachectl configtest"
 # 	@ssh $(TARGET_SERVER_USER)@$(TARGET_SERVER_ADDR) "sudo a2ensite $(APP_NAME)*"
@@ -204,12 +136,6 @@ endif
 # 	@echo "--- INSIDE /mnt/storage_sistemas/$(APP_NAME)/:"
 # 	@ls --color=auto -lah /mnt/storage_sistemas/$(APP_NAME)/
 
-# post-receive:
-# 	clear
-# 	date
-# 	echo "abcdef123 fedcba987 refs/heads/master" | ./git-hooks/post-receive
-# 	date
-
 # build-ctags:
 # 	@cd backoffice
 # 	@ctags -e -R --exclude=.git --exclude=__pycache__ --exclude=tests --exclude=venv --exclude=static --exclude=media --exclude=.mypy_cache --exclude=*~ .
@@ -229,20 +155,3 @@ endif
 # 	@date
 # 	ps aux | grep firefox | grep -v grep | awk '{print $2}' | sudo xargs kill -9
 # 	@date
-
-SEARCH_SRC_STR ?= "FORGE_SYSTEM_NAME"
-SEARCH_TYPE ?= "SUMMARY"
-search-in-source-code:
-	@clear
-	@date
-	@echo "SEARCH_TYPE --> $(SEARCH_TYPE) ::: SEARCH SRC STR --> $(SEARCH_SRC_STR)"
-ifeq ($(SEARCH_TYPE),SUMMARY)
-	@grep -rn "$(SEARCH_SRC_STR)" * --exclude-dir={forge,tmp,venv,__pycache__,tests} --exclude={TAGS,dev.patch} | awk '{print $1}'
-else ifeq ($(SEARCH_TYPE),FULL)
-	@grep -rn "$(SEARCH_SRC_STR)" * --exclude-dir={forge,tmp,venv,__pycache__,tests} --exclude={TAGS,dev.patch} | awk '{print $1}' | grep -v "~" | grep -v ":from" | sort | uniq
-endif
-	@echo "-------"
-	@grep -rn "$(SEARCH_SRC_STR)" * --exclude-dir={forge,tmp,venv,__pycache__,tests} --exclude={TAGS,dev.patch} | awk '{print $1}' | sort | uniq | wc -l
-	@echo ""
-	@date
-
--- main.sh
+++ main.sh
@@ -10,154 +10,55 @@ then
 fi
 
 source $FORGE_PATH/var.sh
+source $FORGE_PATH/utils.sh
 source $FORGE_PATH/env.sh
 source $FORGE_PATH/database.sh
 source $FORGE_PATH/deployment.sh
 source $FORGE_PATH/monitoring.sh
 
-# TODO implement bash completation
-case $1 in
-    "show")
-        clear
-        show_env $2
-        ;;
-    "unenv")
-        unset_symbolic_link
-        unset_vars
-        show_env $2
-        ;;
-    "env")
-        case $2 in
-            "local" | "dev" | "stage" | "prod")
-                unset_vars
-                set_vars $2 "$3" "$4"
-                set_vars_by_env
-                set_symbolic_link
-                show_env "PWD"
-                ;;
-            *)
-                echo "ENV USAGE: [local | dev | stage | prod]. $1 *NOT* found!!"
-                ;;
-        esac
-        ;;
-    "genenv")
-		# TODO use var WORKFLOW_ENVS_AVAILABLE in code bellow
-		echo "${WORKFLOW_ENVS_AVAILABLE[@]}"
-		for ENV_TRG in ${ENVS_AVAILABLE[@]};
-		do
-			# if [[ "$2" == ${WORKFLOW_ENVS_AVAILABLE[@]} ]];
-			if [[ "$2" == @(local|dev|stage|prod) ]];
-			then
-				echo "IS VALID ENV ::: $2"
-				# generate_conf_file $2
-				break
-			fi
-
-<<<<<<< Updated upstream
-			if [[ "$ENV_TRG" != @(edge|upstream) && "$2" == @(all|ALL) ]];
-			then
-				echo ""
-				echo "************************************"
-				echo "|| Genereting env files for $ENV_TRG"
-				echo "************************************"
-				echo ""
-				# generate_conf_file $ENV_TRG
-			fi
-		done
-        ;;
-    # "githook") githook $2 $3 $4;;
-    "deploy") deploy;;
-    "terraform")
-        terraform $2
-        ;;
-    "is_mounted") verify_mounted_path_online $2 $3;;
-    "db_script")
-        case $3 in
-            "admin" | "adm")
-                db_script "$DB_ADMIN_HOST" "$DB_ADMIN_PORT" "$DB_ADMIN_DATABASE" "$DB_ADMIN_USER" $2
-                ;;
-            *)
-                db_script "$DB_HOST" "$DB_PORT" "$DB_DATABASE" "$DB_USER" $2
-                ;;
-        esac
-        ;;
-    "db_backup")
-        case $3 in
-            "full")
-                db_backup_full $DB_DATABASE
-                ;;
-            "partial")
-                db_backup_partial $DB_DATABASE $DB_USER
-                ;;
-            *)
-                echo "No backup was recognized: $3"
-                ;;
-        esac
-        ;;
-    "set_symbolic_link") set_symbolic_link;; # FIXME temp only to test
-    *)
-        # TODO better usage message
-        echo "-----------"
-        echo "[FORGE] $(dirname $0)"
-        echo "[FORGE] $FORGE_PATH"
-        echo "-----------"
-||||||| Stash base
-			    if [[ "$ENV_TRG" != @(edge|upstream) && "$2" == @(all|ALL) ]];
+main() {
+    # TODO implement bash completation
+    case $1 in
+        "show")
+            clear
+            show_env $2 $3
+            ;;
+        "unenv")
+            unset_symbolic_link
+            unset_vars
+            show_env $2
+            ;;
+        "env")
+            case $2 in
+                "local" | "dev" | "stage" | "prod")
+                    unset_vars
+                    set_vars $2 "$3" "$4"
+                    set_vars_by_env
+                    set_symbolic_link
+                    show_env "PWD"
+                    ;;
+                *)
+                    echo "ENV USAGE: [local | dev | stage | prod]. $1 *NOT* found!!"
+                    ;;
+            esac
+            ;;
+        "genenv")
+		    # TODO use var WORKFLOW_ENVS_AVAILABLE in code bellow
+		    # echo "${WORKFLOW_ENVS_AVAILABLE[@]}"
+		    for ENV_TRG in ${ENVS_AVAILABLE[@]};
+		    do
+			    # if [[ "$2" == ${WORKFLOW_ENVS_AVAILABLE[@]} ]];
+			    if [[ "$2" == @(local|dev|stage|prod) ]];
 			    then
 				    echo ""
 				    echo "************************************"
-				    echo "|| Genereting env files for $ENV_TRG"
+				    echo "|| Genereting env for valid: $2"
 				    echo "************************************"
 				    echo ""
-				    generate_conf_file $ENV_TRG $3
+				    generate_conf_file $2 $3
+				    break
 			    fi
-		    done
-            ;;
-	    "cp-secrets")
-		    cp_secrets $2
-		    ;;
-        # "githook") githook $2 $3 $4;;
-        "deploy")
-		    deploy
-		    ;;
-        "etc_terraform") terraform_app_path_etc;;
-        "apache_terraform") terraform_app_path_var_www_app;;
-        "terraform")
-            terraform $2
-            ;;
-        "is_mounted")
-		    verify_mounted_path_online $2 $3
-		    ;;
-        "db_script")
-            case $3 in
-                "admin" | "adm")
-                    db_script "$DB_ADMIN_HOST" "$DB_ADMIN_PORT" "$DB_ADMIN_DATABASE" "$DB_ADMIN_USER" $2
-                    ;;
-                *)
-                    db_script "$DB_HOST" "$DB_PORT" "$DB_DATABASE" "$DB_USER" $2
-                    ;;
-            esac
-            ;;
-        "db_backup")
-            case $3 in
-                "full")
-                    db_backup_full $DB_DATABASE
-                    ;;
-                "partial")
-                    db_backup_partial $DB_DATABASE $DB_USER
-                    ;;
-                *)
-                    echo "No backup was recognized: $3"
-                    ;;
-            esac
-            ;;
-        *)
-            # TODO better usage message
-            echo "-----------"
-            echo "[FORGE] $(dirname $0)"
-            echo "[FORGE] $FORGE_PATH"
-            echo "-----------"
-=======
+
 			    if [[ "$ENV_TRG" != @(edge|upstream) && "$2" == @(all|ALL) ]];
 			    then
 				    echo ""
@@ -215,17 +116,20 @@ case $1 in
             echo "[FORGE] $(dirname $0)"
             echo "[FORGE] $FORGE_PATH"
             echo "-----------"
->>>>>>> Stashed changes
 
-        echo "USAGE: [show | unenv | env | githook | terraform]. $1 *NOT* found!!"
-        echo "- show [PWD | \"\"]"
-        echo "- unenv"
-        echo "- env [local | dev | stage | prod] <OPTIONAL_GIT_REPOS> <OPTIONAL_GIT_BRANCH>. GIT_REPOS default is backend; GIT_BRANCH default is same as TARGET_ENV: (now is $TARGET_ENV)."
-        echo "- genenv [local | dev | stage | prod | all]"
-        # echo "- githook - only used by post-receive script"
-        echo "- deploy"
-        echo "- terraform - prepare devops"
-        echo "- is_mounted - validate if mount point is online"
-        echo "- db_script <DB_SCRIPT> [admin|adm|""] - execute <DB_SCRIPT> as admin or not"
-        echo "- db_backup_full - create a backup full from database"
-esac
+            echo "[FORGE] USAGE: [show | unenv | env | githook | terraform]. $1 *NOT* found!!"
+            echo "- show [PWD | \"\"]"
+            echo "- unenv"
+            echo "- env [local | dev | stage | prod] <OPTIONAL_GIT_REPOS> <OPTIONAL_GIT_BRANCH>. GIT_REPOS default is backend; GIT_BRANCH default is same as TARGET_ENV: (now is $TARGET_ENV)."
+            echo "- genenv [local | dev | stage | prod | all]"
+            # echo "- githook - only used by post-receive script"
+            echo "- deploy"
+            echo "- terraform - prepare devops"
+            echo "- is_mounted - validate if mount point is online"
+            echo "- db_script <DB_SCRIPT> [admin|adm|""] - execute <DB_SCRIPT> as admin or not"
+            echo "- db_backup_full - create a backup full from database"
+            # return 1 # FIXME should return an error here?!
+    esac
+
+    return 0
+}
