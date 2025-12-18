bf5ba7209aa9811f4302daeb1d03972c1cf06267
Jefferson Campos <jecampos@sorocaba.sp.gov.br>
2025-12-18 13:16:31 -0300

    feat: New Database Tasks.
    Imported from river.

--- database.mk
+++ database.mk
@@ -7,6 +7,9 @@ db-script:
 db-start: db-terraform db-ddl db-permission django-update-permissions django-users db-fixtures
 	@date
 
+db-dev: db-terraform db-refresh-migration db-ddl db-permission django-super-user db-fixtures
+	@date
+
 db-deploy: db-terraform db-ddl db-permission db-seeds
 	@echo "|+-------------------------+|"
 	@echo "| DEPLOY PROD FROM SCRATCH  |"
@@ -18,76 +21,90 @@ db-terraform:
 	@echo "| TERRAFORMING  |"
 	@echo "|+-------------+|"
 	@psql -v forgesys_path="$(shell pwd)" -h $(DB_ADMIN_HOST) -p $(DB_ADMIN_PORT) -d $(DB_ADMIN_DATABASE) -U $(DB_ADMIN_USER) -f database/terraform.sql
-	@echo "|+-------------+|"
-	@echo "| INITIALIZE    |"
-	@echo "|+-------------+|"
+	@echo "|+--------------------+|"
+	@echo "| INITIALIZE           |"
+	@echo "|+--------------------+|"
 	@psql -v forgesys_path="$(shell pwd)" -h $(DB_HOST) -p $(DB_PORT) -d $(DB_DATABASE) -U $(DB_USER) -f database/initialize.sql
-	@echo "|+--------------+|"
-	@echo "| MIGRATE DJANGO |"
-	@echo "|+--------------+|"
+	@echo "|+--------------------+|"
+	@echo "| MIGRATE DJANGO       |"
+	@echo "|+--------------------+|"
 	@python3 backoffice/manage.py migrate
 	@date
 
 db-ddl:
-	@echo "|+-------------+|"
-	@echo "| INDEPENDENTS  |"
-	@echo "|+-------------+|"
+	@echo "|+--------------------+|"
+	@echo "| INDEPENDENTS         |"
+	@echo "|+--------------------+|"
 	@psql -v forgesys_path="$(shell pwd)" -h $(DB_HOST) -p $(DB_PORT) -d $(DB_DATABASE) -U $(DB_USER) -f database/independent.sql
-	@echo "|+-------------+|"
-	@echo "| DDL           |"
-	@echo "|+-------------+|"
+	@echo "|+--------------------+|"
+	@echo "| DDL                  |"
+	@echo "|+--------------------+|"
 	@psql -v forgesys_path="$(shell pwd)" -h $(DB_HOST) -p $(DB_PORT) -d $(DB_DATABASE) -U $(DB_USER) -f database/ddl.sql
 
 db-permission:
-	@echo "|+-------------+|"
-	@echo "| PERMISSION    |"
-	@echo "|+-------------+|"
+	@echo "|+--------------------+|"
+	@echo "| PERMISSION           |"
+	@echo "|+--------------------+|"
 	@psql -v forgesys_path="$(shell pwd)" -h $(DB_HOST) -p $(DB_PORT) -d $(DB_DATABASE) -U $(DB_USER) -f database/permissions.sql
 
+import-permission-superuser:
+	@echo "|+--------------------+|"
+	@echo "| PERMISSION SUPERUSER |"
+	@echo "|+--------------------+|"
+	@psql -h $(DB_HOST) -p $(DB_PORT) -d $(DB_DATABASE) -U $(DB_USER) -f database/permissions_superuser.sql
+
 db-seeds:
-	@echo "|+-------------+|"
-	@echo "| SEEDS         |"
-	@echo "|+-------------+|"
+	@echo "|+--------------------+|"
+	@echo "| SEEDS                |"
+	@echo "|+--------------------+|"
 	@psql -v forgesys_path="$(shell pwd)" -h $(DB_HOST) -p $(DB_PORT) -d $(DB_DATABASE) -U $(DB_USER) -f database/seeds.sql
 
 db-fixtures: db-seeds
-	@echo "|+-------------+|"
-	@echo "| FIXTURES      |"
-	@echo "|+-------------+|"
+	@echo "|+--------------------+|"
+	@echo "| FIXTURES             |"
+	@echo "|+--------------------+|"
 	@psql -v forgesys_path="$(shell pwd)" -h $(DB_HOST) -p $(DB_PORT) -d $(DB_DATABASE) -U $(DB_USER) -f database/fixtures.sql
 
 db-show:
-	@echo "|+-------------+|"
-	@echo "| SHOW          |"
-	@echo "|+-------------+|"
+	@echo "|+--------------------+|"
+	@echo "| SHOW                 |"
+	@echo "|+--------------------+|"
 	@psql -v forgesys_path="$(shell pwd)" -h $(DB_HOST) -p $(DB_PORT) -d $(DB_DATABASE) -U $(DB_USER) -f database/show_types.sql
 	@psql -v forgesys_path="$(shell pwd)" -h $(DB_ADMIN_HOST) -p $(DB_ADMIN_PORT) -d $(DB_ADMIN_DATABASE) -U $(DB_ADMIN_USER) -c 'SELECT unnest(enum_range(NULL::alerta_defesa_civil.grau_risco)) AS "Grau de Risco", unnest(enum_range(NULL::alerta_defesa_civil.grau_risco_tipologia_estudo)) AS "Gr Risco Tip Est", unnest(enum_range(NULL::alerta_defesa_civil.grau_vulnerabilidade_topologia_estudo)) AS "Gr Vuln Top Est", unnest(enum_range(NULL::alerta_defesa_civil.intervencao)) AS "Intervenção", unnest(enum_range(NULL::alerta_defesa_civil.natureza)) AS "Natureza";'
 
 db-migrate:
-	@echo "|+-------------+|"
-	@echo "| MIGRATE SQL   |"
-	@echo "|+-------------+|"
+	@echo "|+--------------------+|"
+	@echo "| MIGRATE SQL          |"
+	@echo "|+--------------------+|"
 	@psql -v forgesys_path="$(shell pwd)" -h $(DB_HOST) -p $(DB_PORT) -d $(DB_DATABASE) -U $(DB_USER) -f database/migrate.sql
 
 MIGRATION_ENTITY ?= "<SET_YOUR_VAR_MIGRATION_ENTITY_TO_RUN_MAKEFILE_REFRESH_MIGRATION>"
-refresh-migration:
-	@clear
-	@date
+db-refresh-migration:
+	@echo "|+--------------------+|"
+	@echo "| REFRESH MIGRATE      |"
+	@echo "|+--------------------+|"
 	@python3 backoffice/manage.py migrate $(MIGRATION_ENTITY) zero
 	@rm backoffice/$(MIGRATION_ENTITY)/migrations/0001_initial.py
 	@python3 backoffice/manage.py makemigrations
 	@python3 backoffice/manage.py migrate $(MIGRATION_ENTITY)
-	@date
 
-clean-db:
+db-clean:
 	@clear
 	@date
-	@echo "|+-------------+|"
-	@echo "| CLEAN DB SOFT |"
-	@echo "|+-------------+|"
+	@echo "|+--------------------+|"
+	@echo "| CLEAN DB SOFT        |"
+	@echo "|+--------------------+|"
 	@psql -v forgesys_path="$(shell pwd)" -h $(DB_HOST) -p $(DB_PORT) -d $(DB_DATABASE) -U $(DB_USER) -f database/delete.sql
 	@date
 
-# TODO make populate part of django commands
-clean-db-dev: clean-db db-fixtures
-	@date
+db-drop:
+	@echo "|+--------------------+|"
+	@echo "| DROPPING             |"
+	@echo "|+--------------------+|"
+	@psql -h $(DB_HOST) -p $(DB_PORT) -d $(DB_DATABASE) -U $(DB_USER) -f database/drop.sql
+
+# TODO review it
+db-tables-locked:
+	# @psql -h $(DB_HOST) -p $(DB_PORT) -d $(DB_DATABASE) -U $(DB_USER) -c "SELECT pid FROM pg_locks AS l INNER JOIN pg_class AS t ON l.relation = t.oid AND t.relkind = 'r' WHERE t.relname = 'Bill';" | tail -n +3 | head -n -2 | xargs kill
+	@psql -h $(DB_HOST) -p $(DB_PORT) -d $(DB_DATABASE) -U $(DB_USER) -c "SELECT pid FROM pg_locks AS l INNER JOIN pg_class AS t ON l.relation = t.oid AND t.relkind = 'r';" | tail -n +3 | head -n -2
+
