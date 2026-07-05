9cfa3a8efcaf33d2eee967274fd3d389f88e104d
Jefferson Campos <jefferson@jeffersoncampos.eti.br>
2026-07-05 16:34:22 -0300

    feat: Install.
    Vault Validation.

--- install.sh
+++ install.sh
@@ -8,9 +8,17 @@ GIT_BRANCH=${3:-"master"}
 FORGE_SOURCE=${4:-"repos"}         # options: repos (do nothing) or upstream (force update to HEAD)
 FORGE_ORGANIZATION_VAULT=${5:-"secrets"}
 
-echo "--------------------------- forge-install.sh ==> $PROJECT_DIR ---------------------------"
-echo "${FORGE_SYSTEM_ACRONYM} ${FORGE_ORGANIZATION_ACRONYM} ${GIT_BRANCH} ${FORGE_ORGANIZATION_VAULT}"
-echo "--------------------------- forge-install.sh ==> $PROJECT_DIR ---------------------------"
+install_banner() {
+	# Same as original print_banner helper function but lives here because all dependencies must live together and be accessible by one file.
+    BANNER_MSG=$1
+    CHARS_BANNER_MSG=${#BANNER_MSG}
+    BORDER=$(printf -- '-%.0s' $(seq 1 $(($CHARS_BANNER_MSG))))
+    echo ""
+    echo "|+$BORDER+|"
+    echo "| $BANNER_MSG |"
+    echo "|+$BORDER+|"
+    echo""
+}
 
 run() {
     # Get the project and install it for dev env
@@ -18,49 +26,68 @@ run() {
     # [MANDATORY] FORGE_SYSTEM_ACRONYM..................: To choose the project that will be installed.
     # [MANDATORY] FORGE_ORGANIZATION_ACRONYM............: To compose the default path of your app.
     # [OPTIONAL ] GIT_BRANCH............................: To choose wich branch will checkedout. Normally you can use [master | stage | dev | local].
-    # [OPTIONAL ] FORGE_ORGANIZATION_VAULT..............: The custom path to vault. Is assumed that will live inside $HOME/universal/projects/${FORGE_ORGANIZATION_ACRONYM}/${FORGE_ORGANIZATION_VAULT}.
     # [OPTIONAL ] FORGE_SOURCE..........................: Choose between 'upstream' (to force submodule forge in project target to be updated to HEAD of master) or 'repos' - or any other value - to use the forge as is.
+    # [OPTIONAL ] FORGE_ORGANIZATION_VAULT..............: The custom url to vault, if needed. Is assumed that will live inside $HOME/universal/projects/${FORGE_ORGANIZATION_ACRONYM}/${FORGE_ORGANIZATION_VAULT}.
 
-    PROJECT_DIR="${HOME}/universal/projects/${FORGE_ORGANIZATION_ACRONYM}/${FORGE_SYSTEM_ACRONYM}"
+    local PROJECT_DIR="${HOME}/universal/projects/${FORGE_ORGANIZATION_ACRONYM}/${FORGE_SYSTEM_ACRONYM}"
+	install_banner "[FORGE_INSTALL] VALIDATING PROJECT_DIR IN $PROJECT_DIR"
     if [[ -e "$PROJECT_DIR" ]];
     then
-        echo "NOT GOING FUTHER: $PROJECT_DIR ALREADY exist!!"
+        echo "[FORGE_INSTALL] NOT GOING FUTHER: $PROJECT_DIR ALREADY exist!!"
         return 1
     fi
-    mkdir -p "$PROJECT_DIR"
 
-    # TODO move centralized vault to /usr/share or something more posix-friendly. Also, give the ability to download it if necessary
-    export MISE_EN_PLACE_ENCRYPTED_PATH="$HOME/universal/projects/${FORGE_ORGANIZATION_ACRONYM}/${FORGE_ORGANIZATION_VAULT}/code/encrypted/${FORGE_SYSTEM_ACRONYM}/.mise-en-place.conf.gpg"
+    local VAULT_PATH="$HOME/universal/projects/${FORGE_ORGANIZATION_ACRONYM}/secrets"
+	install_banner "[FORGE_INSTALL] VALIDATING VAULT IN $VAULT_PATH"
+	if [[ ! -d "$VAULT_PATH/code" ]];
+	then
+		(git clone "${FORGE_ORGANIZATION_VAULT}" "${VAULT_PATH}/code") >/dev/null 2>&1
+	else
+		(cd "$VAULT_PATH/code" && git pull) >/dev/null 2>&1
+	fi
+
+	# TODO move centralized vault to /usr/share or something more posix-friendly
+    local MISE_EN_PLACE_ENCRYPTED_PATH="${VAULT_PATH}/code/encrypted/${FORGE_SYSTEM_ACRONYM}/.mise-en-place.conf.gpg"
+	install_banner "[FORGE_INSTALL] VALIDATING MISE-EN-PLACE IN $MISE_EN_PLACE_ENCRYPTED_PATH"
     if [[ ! -f "${MISE_EN_PLACE_ENCRYPTED_PATH}" ]];
     then
-        echo "NOT GOING FUTHER: MISSING mise en place configuration file in path ${MISE_EN_PLACE_ENCRYPTED_PATH}"
+        echo "[FORGE_INSTALL] NOT GOING FUTHER: MISSING mise en place configuration file in path ${MISE_EN_PLACE_ENCRYPTED_PATH}"
         return 1
     fi
-    export VAULT_SENSIBLE_PATH=$HOME/universal/projects/${FORGE_ORGANIZATION_ACRONYM}/${FORGE_ORGANIZATION_VAULT}/.credentials_backup/original/${FORGE_SYSTEM_ACRONYM}
-    mkdir -p "${VAULT_SENSIBLE_PATH}" && gpg --yes -o "${VAULT_SENSIBLE_PATH}/.mise-en-place.conf" -d $MISE_EN_PLACE_ENCRYPTED_PATH
+    local VAULT_SENSIBLE_PATH="${VAULT_PATH}/.forge_workspace/original/${FORGE_SYSTEM_ACRONYM}"
+	# TODO add all gpg keys in keyring -> ideas: export trust-file db and sign all keys
+    mkdir -p "${VAULT_SENSIBLE_PATH}" && gpg -q --yes -o "${VAULT_SENSIBLE_PATH}/.mise-en-place.conf" -d $MISE_EN_PLACE_ENCRYPTED_PATH
 
-    FORGE_SYSTEM_BASE_DNS=$(cat ${VAULT_SENSIBLE_PATH}/.mise-en-place.conf | grep FORGE_SYSTEM_BASE_DNS | cut -d = -f2)
-    GIT_PROTOCOL=$(cat ${VAULT_SENSIBLE_PATH}/.mise-en-place.conf | grep GIT_PROTOCOL | cut -d = -f2)
-    GIT_BASE_URL=$(cat ${VAULT_SENSIBLE_PATH}/.mise-en-place.conf | grep GIT_BASE_URL | cut -d = -f2)
-    GIT_USER=$(cat ${VAULT_SENSIBLE_PATH}/.mise-en-place.conf | grep GIT_USER | cut -d = -f2)
-    GIT_PASSWORD=$(cat ${VAULT_SENSIBLE_PATH}/.mise-en-place.conf | grep GIT_PASSWORD | cut -d = -f2)
-    GIT_REMOTE="${GIT_PROTOCOL}${GIT_USER}@${GIT_BASE_URL}/${FORGE_SYSTEM_BASE_DNS}.git"
+	install_banner "[FORGE_INSTALL] CLONING THE PROJECT..."
+    local FORGE_SYSTEM_BASE_DNS=$(cat ${VAULT_SENSIBLE_PATH}/.mise-en-place.conf | grep FORGE_SYSTEM_BASE_DNS | cut -d = -f2)
+    local GIT_PROTOCOL=$(cat ${VAULT_SENSIBLE_PATH}/.mise-en-place.conf | grep GIT_PROTOCOL | cut -d = -f2)
+    local GIT_BASE_URL=$(cat ${VAULT_SENSIBLE_PATH}/.mise-en-place.conf | grep GIT_BASE_URL | cut -d = -f2)
+    local GIT_USER=$(cat ${VAULT_SENSIBLE_PATH}/.mise-en-place.conf | grep GIT_USER | cut -d = -f2)
+    local GIT_REMOTE="${GIT_PROTOCOL}${GIT_USER}@${GIT_BASE_URL}/${FORGE_SYSTEM_BASE_DNS}.git"
+    export GIT_PASSWORD=$(cat ${VAULT_SENSIBLE_PATH}/.mise-en-place.conf | grep GIT_PASSWORD | cut -d = -f2) # side-note: need to be availiable to sub-shell
+	if [[ "$DEBUG" == "1" ]];
+    then
+		echo "FORGE_SYSTEM_BASE_DNS..: $FORGE_SYSTEM_BASE_DNS"
+		echo "GIT_PROTOCOL...........: $GIT_PROTOCOL"
+		echo "GIT_BASE_URL...........: $GIT_BASE_URL"
+		echo "GIT_USER...............: $GIT_USER"
+		echo "GIT_REMOTE.............: $GIT_REMOTE"
+		echo "GIT_PASSWORD...........: $GIT_PASSWORD"
+    fi
+	mkdir -p "$PROJECT_DIR"
     git -c credential.helper='!f() { sleep 1; echo "password=${GIT_PASSWORD}"; }; f' clone -b ${GIT_BRANCH} --recurse-submodules $GIT_REMOTE "${PROJECT_DIR}/backend"
-
-    # TODO add all gpg keys in keyring -> ideas: export trust-file db and sign all keys
     cp "${VAULT_SENSIBLE_PATH}/.mise-en-place.conf" "${PROJECT_DIR}/backend/.credentials/.mise-en-place.conf"
 
-    echo "----- Updating forge (The value 'upstream' will update repository to HEAD from 'master'; 'repos' or any other value stay as is checked out): $FORGE_SOURCE -----"
-    [[ "$FORGE_SOURCE" == "upstream" ]] && cd $PROJECT_DIR/backend/forge && git pull origin master
+    install_banner "----- Updating forge ('upstream' goes to HEAD from 'master'; 'repos' or any will be as is): $FORGE_SOURCE -----"
+    [[ "$FORGE_SOURCE" == "upstream" ]] && cd $PROJECT_DIR/backend/forge && git pull origin master >/dev/null 2>&1
     cd $PROJECT_DIR/backend && source ${PROJECT_DIR}/backend/forge/main.sh
-    print_banner "Erupt dev_terraform local on $(pwd) ..."
+    install_banner "[FORGE_INSTALL] Erupt dev_terraform local on $(pwd) ..."
     erupt dev_terraform local
+
+	return 0
 }
 
-echo "(1) system acronym: ${FORGE_SYSTEM_ACRONYM} and (2) organization acronym: ${FORGE_ORGANIZATION_ACRONYM}. ALSO, FOUND GIT_BRANCH AS $GIT_BRANCH :: ALSO, FOUND FORGE_ORGANIZATION_VAULT AS $FORGE_ORGANIZATION_VAULT"
-if [[ ! -z "$FORGE_SYSTEM_ACRONYM" && ! -z "$FORGE_ORGANIZATION_ACRONYM" ]];
-then
-    run
-else
-    echo "NOT EXECUTING: MANDATORY VARS MISSING!!"
-fi
+echo "--------------------------- forge-install.sh ==> $PROJECT_DIR ---------------------------"
+echo "${FORGE_SYSTEM_ACRONYM} ${FORGE_ORGANIZATION_ACRONYM} ::: ${GIT_BRANCH} ${FORGE_SOURCE} ${FORGE_ORGANIZATION_VAULT}"
+echo "--------------------------- forge-install.sh ==> $PROJECT_DIR ---------------------------"
+[[ ! -z "$FORGE_SYSTEM_ACRONYM" && ! -z "$FORGE_ORGANIZATION_ACRONYM" ]] && run || echo "[FORGE_INSTALL] NOT EXECUTING: MANDATORY VARS MISSING OR SOMETHING GONE WRONG!!"
