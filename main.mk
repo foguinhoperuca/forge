a6fffbc19bbbaa59a5ac776be77eb2dfe9a2717f
Jefferson Campos <jefferson@jeffersoncampos.eti.br>
2026-08-27 17:55:08 -0300

    doc: post-receive.
    Fixed doc.

--- main.mk
+++ main.mk
@@ -88,7 +88,7 @@ post-receive:
 	git commit --allow-empty -m "test: git hook post receive trigger"
 	# git push deployment $(TARGET_ENV)
 	# git config alias.deploy-force push deployment HEAD:refs/triggers/deploy --force
-	# git config push deployment :refs/triggers/deploy --force && git config push deployment HEAD:refs/triggers/deploy --force # DELETE and RE-CREATE namespace
+	# git push deployment :refs/triggers/deploy --force && git push deployment HEAD:refs/triggers/deploy --force # DELETE and RE-CREATE namespace
 	echo "abcdef123 fedcba987 refs/heads/$(TARGET_ENV)" | ./git-hooks/post-receive
 	@date
 
